dadf35b8136f1bf5953578e130ef4c9a099d3a56
Manish Saini <mk81895@gmail.com>
2020-07-13 20:31:10 +0530

    Added react-route-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { Link, BrowserRouter as Router } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import { fetchPosts } from '../actions/posts';
-import { PostsList } from './';
+import { fetchPosts } from "../actions/posts";
+import { PostsList, Navbar } from "./";
 
 class App extends React.Component {
   componentDidMount() {
@@ -12,9 +14,12 @@ class App extends React.Component {
   render() {
     const { posts } = this.props;
     return (
+      <Router>
       <div>
+        <Navbar />
         <PostsList posts={posts} />
       </div>
+      </Router> 
     );
   }
 }
@@ -24,4 +29,9 @@ function mapStateToProps(state) {
     posts: state.posts,
   };
 }
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
 export default connect(mapStateToProps)(App);
