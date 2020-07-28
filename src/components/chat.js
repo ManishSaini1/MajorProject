2b2eb9e32d33729c78b3ef100115b54b53e91dec
Manish Saini <mk81895@gmail.com>
2020-07-29 01:16:13 +0530

    Added the functionality of Chat Box

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "../chat.css";
+import { connect } from "react-redux";
+import io from "socket.io-client";
 
 class chat extends Component {
   constructor(props) {
@@ -8,18 +10,70 @@ class chat extends Component {
       messages: [],
       typedMessage: "",
     };
+
+    this.socket = io.connect("http://54.237.158.65:5000");
+    this.userEmail = props.user.email;
+
+    if (this.userEmail) {
+      this.setUpConnection();
+    }
   }
+  setUpConnection = () => {
+    const socketConnection = this.socket;
+    const self = this;
+    this.socket.on("connect", function () {
+      console.log("Connection Established");
+      socketConnection.emit("join_room", {
+        user_email: this.userEmail,
+        chatroom: "codeial",
+      });
+      socketConnection.on("user_joined", function (data) {
+        console.log("New User Joined", data);
+        console.log("Inside the function", this);
+      });
+    });
+    this.socket.on("receive_message", function (data) {
+      const { messages } = self.state;
+      const messageObject = {};
+      messageObject.content = data.message;
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: "",
+      });
+    });
+  };
+  handleSubmit = () => {
+    // Set-Cookie: flavor=choco; SameSite=None; Secure;
+    console.log("Inside the Chat Function handleSubmit");
+    const { typedMessage } = this.state;
+    if (typedMessage && this.userEmail) {
+      this.socket.emit("send_message", {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: "codeial",
+      });
+    }
+  };
   render() {
     const { typedMessage, messages } = this.state;
+  
     return (
       <div className="chat-container">
         <div className="chat-header">
           Chat
-          <img src="https://image.flaticon.com/icons/svg/982/982629.svg" alt="" height={17} />
+          <img
+            src="https://image.flaticon.com/icons/svg/982/982629.svg"
+            alt=""
+            height={17}
+          />
         </div>
         <div className="chat-messages">
           {messages.map((message) => (
             <div
+            
               className={
                 message.self
                   ? "chat-bubble self-chat"
@@ -27,6 +81,8 @@ class chat extends Component {
               }
             >
               {message.content}
+              
+            
             </div>
           ))}
         </div>
@@ -44,4 +100,9 @@ class chat extends Component {
   }
 }
 
-export default chat;
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+export default connect(mapStateToProps)(chat);
